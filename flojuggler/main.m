86c90274647df8742cf214b8ec54bebf8a15901f
Selino Valdes <selino@svaldes-macbook.local>
2013-10-18 10:57:52 -0700

    Initial Commit

---
+++ flojuggler/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  flojuggler
+//
+//  Created by Selino Valdes on 10/18/13.
+//  Copyright (c) 2013 Selino Valdes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
