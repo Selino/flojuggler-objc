0e0c66c9483e6ab7d2c94f1be963a89360df2b28
Selino Valdes <selino@selino.com>
2013-11-06 14:46:24 -0800

    #13 Set default values in create screen.

--- flojuggler/AppDelegate.m
+++ flojuggler/AppDelegate.m
@@ -112,7 +112,10 @@
     NSError *error = nil;
     _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
     if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
+        
+        //delete store if an error is thrown
         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil];
+        
         /*
          Replace this implementation with code to handle the error appropriately.
          
